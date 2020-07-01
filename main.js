152fdae92b4f644db85bbc1f7dab844cecaf1836
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-01 23:18:18 +0900

    Template Syntax

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
-var data = { a: 1, };
-var vm = new Vue({
-  el: '#example',
-  data: data,
-});
-console.log(vm.a == data.a);
-console.log(vm.a === data.a);
-
-vm.a = 2;
-console.log(vm.a);
-console.log(data.a);
-
-data.a = 3;
-console.log(vm.a);
-console.log(data.a);
-
-vm.b = 'Hi!';
-console.log(vm.b);
-console.log(data.b);
-
-/****************/
-
-var obj = {
-  foo: 'bar',
-};
-// Object.freeze(obj);
-new Vue({
+var app = new Vue({
   el: '#app',
-  data: obj,
-});
-
-/****************/
-
-new Vue({
   data: {
-    a: 1,
-  },
-  created: function() {
-    console.log('a is: ' + this.a);
+    msg: 'Hello World!',
   },
 });
 
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    msg2: 'Hello again!',
+  },
+});
 
+/**********/
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    rawHtml: '<span style="color: red;">This should be red.</span>'
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    rawHtml: '<span style="color: red;">This should be red.</span>'
+  }
+});
+
+/**********/
+
+// var square = new Vue({
+//   el: '#square',
+//   methods: {
+//     rotate: function() {
+//       this.el.classList.add('rotate');
+//     },
+//   },
+// });
