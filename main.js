6881d3095e79f783227fd886ec494ef0e26703d2
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-02 22:22:24 +0900

    Class and Style Bindings

--- main.js
+++ main.js
@@ -1,89 +1,31 @@
-var vm = new Vue({
-  el: '#example',
+app = new Vue({
+  el: '#app',
   data: {
-    message: 'Hello',
-  },
-  computed: {
-    reversedMessage: function() {
-      return this.message.split('').reverse().join('');
+    classObject: {
+      active: true,
+      'text-danger': false,
     }
   },
 });
 
-// var vm2 = new Vue({
-//   el: '#demo',
-//   data: {
-//     firstName: 'Foo',
-//     lastName: 'Bar',
-//     fullName: 'Foo Bar',
-//   },
-//   watch: {
-//     firstName: function(val) {
-//       this.fullName = val + ' ' + this.lastName;
-//     },
-//     lastName: function(val) {
-//       this.fullName = this.firstName + ' ' + val;
-//     },
-//   },
-// });
-
-var vm2 = new Vue({
-  el: '#demo',
+app2 = new Vue({
+  el: '#app-2',
   data: {
-    firstName: 'Foo',
-    lastName: 'Bar',
+    activeClass: 'active',
+    errorClass: 'text-danger',
   },
-  computed: {
-    fullName: {
-      get: function() {
-        return this.firstName + ' ' + this.lastName;
-      },
-      set: function(newValue) {
-        var names = newValue.split(' ');
-        this.firstName = names[0];
-        this.lastName = names[names.length - 1];
-      },
+});
+
+app3 = new Vue({
+  el: '#app-3',
+  data: {
+    styleObject: {
+      color: 'blue',
+      fontSize: '24px',
     },
+    otherStyles: {
+      fontWeight: 800,
+      letterSpacing: '24px',
+    }
   },
 });
-
-var watchExampleVM = new Vue({
-  el: '#watch-example',
-  data: {
-    question: '',
-    answer: 'I cannot give you an answer until you ask a question!'
-  },
-  watch: {
-    // この関数は question が変わるごとに実行されます。
-    question: function (newQuestion, oldQuestion) {
-      this.answer = 'Waiting for you to stop typing...'
-      this.debouncedGetAnswer()
-    }
-  },
-  created: function () {
-    // _.debounce は特にコストの高い処理の実行を制御するための
-    // lodash の関数です。この場合は、どのくらい頻繁に yesno.wtf/api
-    // へのアクセスすべきかを制限するために、ユーザーの入力が完全に
-    // 終わるのを待ってから ajax リクエストを実行しています。
-    // _.debounce (とその親戚である _.throttle )  についての詳細は
-    // https://lodash.com/docs#debounce を見てください。
-    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
-  },
-  methods: {
-    getAnswer: function () {
-      if (this.question.indexOf('?') === -1) {
-        this.answer = 'Questions usually contain a question mark. ;-)'
-        return
-      }
-      this.answer = 'Thinking...'
-      var vm = this
-      axios.get('https://yesno.wtf/api')
-        .then(function (response) {
-          vm.answer = _.capitalize(response.data.answer)
-        })
-        .catch(function (error) {
-          vm.answer = 'Error! Could not reach the API. ' + error
-        })
-    }
-  }
-})
