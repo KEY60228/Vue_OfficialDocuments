5f7362f6cf0a14b27beebf2c38cedab730799a61
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-03 21:46:47 +0900

    Conditional Rendering

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-app = new Vue({
+let app = new Vue({
   el: '#app',
   data: {
-    classObject: {
-      active: true,
-      'text-danger': false,
-    }
-  },
-});
-
-app2 = new Vue({
-  el: '#app-2',
-  data: {
-    activeClass: 'active',
-    errorClass: 'text-danger',
-  },
-});
-
-app3 = new Vue({
-  el: '#app-3',
-  data: {
-    styleObject: {
-      color: 'blue',
-      fontSize: '24px',
-    },
-    otherStyles: {
-      fontWeight: 800,
-      letterSpacing: '24px',
-    }
+    awesome: true,
+    char: 'C',
+    loginType: 'username',
   },
 });
