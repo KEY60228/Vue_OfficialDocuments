b6e9ae16659eb708bcfc418b913f8d731ac06e80
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-05 14:49:30 +0900

    Props

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-const ComponentA = {
-  props: ['title'],
-  template: `
-    <h1>{{ title }}</h1>
-  `, 
+const compA = {
+  props: {
+    title: String,
+    likes: Number,
+    isPublished: Boolean,
+    commentIds: Array,
+    author: Object,
+    callback: Function,
+    contactsPromise: Promise,
+  },
+  template: '<h3>{{ title }}</h3>'
 };
 
-const app1 = new Vue({
-  el: '#app',
+const app = new Vue({
+  el:'#app',
   data: {
-    title: 'Hello, World!',
+    post: { 
+      id: 1, 
+      title: 'Hello', 
+      likes: 39, 
+      isPublished: true, 
+      commentIds: ['GUCCI', 'PORI'], 
+      author: {
+        name: 'GUCCI'
+      }, 
+      callback: function() {
+        alert('Hi!')
+      },
+    },
   },
   components: {
-    'component-a': ComponentA,
-  },
+    'blog-post': compA,
+  }
 });
