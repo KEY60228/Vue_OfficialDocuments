5c5c1a702821a941f11b8165107e2765185ebf77
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-06 21:34:40 +0900

    Dynamic & Async Components

--- main.js
+++ main.js
@@ -1,68 +1,70 @@
-const compA = {
-  props: ['url', 'user'],
+Vue.component("tab-posts", {
+  data: function() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "Cat Ipsum",
+          content:
+            "<p>Dont wait for the storm to pass, dance in the rain kick up litter decide to want nothing to do with my owner today demand to be let outside at once, and expect owner to wait for me as i think about it cat cat moo moo lick ears lick paws so make meme, make cute face but lick the other cats. Kitty poochy chase imaginary bugs, but stand in front of the computer screen. Sweet beast cat dog hate mouse eat string barf pillow no baths hate everything stare at guinea pigs. My left donut is missing, as is my right loved it, hated it, loved it, hated it scoot butt on the rug cat not kitten around</p>"
+        },
+        {
+          id: 2,
+          title: "Hipster Ipsum",
+          content:
+            "<p>Bushwick blue bottle scenester helvetica ugh, meh four loko. Put a bird on it lumbersexual franzen shabby chic, street art knausgaard trust fund shaman scenester live-edge mixtape taxidermy viral yuccie succulents. Keytar poke bicycle rights, crucifix street art neutra air plant PBR&B hoodie plaid venmo. Tilde swag art party fanny pack vinyl letterpress venmo jean shorts offal mumblecore. Vice blog gentrify mlkshk tattooed occupy snackwave, hoodie craft beer next level migas 8-bit chartreuse. Trust fund food truck drinking vinegar gochujang.</p>"
+        },
+        {
+          id: 3,
+          title: "Cupcake Ipsum",
+          content:
+            "<p>Icing dessert soufflé lollipop chocolate bar sweet tart cake chupa chups. Soufflé marzipan jelly beans croissant toffee marzipan cupcake icing fruitcake. Muffin cake pudding soufflé wafer jelly bear claw sesame snaps marshmallow. Marzipan soufflé croissant lemon drops gingerbread sugar plum lemon drops apple pie gummies. Sweet roll donut oat cake toffee cake. Liquorice candy macaroon toffee cookie marzipan.</p>"
+        }
+      ],
+      selectedPost: null
+    };
+  },
   template: `
-    <a v-bind:href="url" class="nav-link">
-      <slot>Hi! from Vue.</slot>
-    </a>
-  `
-};
+<div class="posts-tab">
+<ul class="posts-sidebar">
+  <li
+    v-for="post in posts"
+    v-bind:key="post.id"
+    v-bind:class="{ selected: post === selectedPost }"
+    v-on:click="selectedPost = post"
+  >
+    {{ post.title }}
+  </li>
+</ul>
+<div class="selected-post-container">
+  <div 
+    v-if="selectedPost"
+    class="selected-post"
+  >
+    <h3>{{ selectedPost.title }}</h3>
+    <div v-html="selectedPost.content"></div>
+  </div>
+  <strong v-else>
+    Click on a blog title to the left to view it.
+  </strong>
+</div>
+</div>
+`
+});
 
-const app = new Vue({
-  el:'#app',
+Vue.component("tab-archive", {
+  template: "<div>Archive component</div>"
+});
+
+new Vue({
+  el: "#dynamic-component-demo",
   data: {
-    user: {
-      name: 'GUCCI',
-    },
+    currentTab: "Posts",
+    tabs: ["Posts", "Archive"]
   },
-  components: {
-    'navigation-link': compA,
-  },
+  computed: {
+    currentTabComponent: function() {
+      return "tab-" + this.currentTab.toLowerCase();
+    }
+  }
 });
-
-const compB = {
-  props: [],
-  template: `
-    <div class="container">
-      <header>
-        <slot name="header"></slot>
-      </header>
-      <main>
-        <slot></slot>
-      </main>
-      <footer>
-        <slot name="footer"></slot>
-      </footer>
-    </div>
-  `,
-};
-
-const app2 = new Vue({
-  el: '#app2',
-  components: {
-    'base-layout': compB,
-  },
-});
-
-// const compC = {
-//   props: [],
-//   template: `
-//     <span>
-//       <slot v-bind:user="user">
-//         {{ user.lastName }}
-//       </slot>
-//     </span>
-//   `,
-// };
-
-// const app3 = new Vue({
-//   el: '#app3',
-//   data: {
-//     user: {
-//       firstName: 'Kenta',
-//       lastName: 'Yamaguchi',
-//     },
-//   },
-//   components: {
-//     'current-user': compC,
-//   },
-// });
