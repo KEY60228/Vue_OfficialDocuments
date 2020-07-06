8de37dc49cdca0549bd8964575a98246089dfd00
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-06 21:16:58 +0900

    Slots

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
 const compA = {
-  model: {
-    prop: 'checked',
-    event: 'change',
-  },
-  props: {
-    checked: Boolean,
-  },
+  props: ['url', 'user'],
   template: `
-    <input type="checkbox" v-bind:checked="checked" v-on:change="$emit('change', $event.target.checked)">
+    <a v-bind:href="url" class="nav-link">
+      <slot>Hi! from Vue.</slot>
+    </a>
   `
 };
 
 const app = new Vue({
   el:'#app',
-  components: {
-    'base-checkbox': compA,
-  },
-  methods: {
-    lovingVue: function() {
-      alert("I Love Vue!");
+  data: {
+    user: {
+      name: 'GUCCI',
     },
   },
+  components: {
+    'navigation-link': compA,
+  },
 });
+
+const compB = {
+  props: [],
+  template: `
+    <div class="container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  `,
+};
+
+const app2 = new Vue({
+  el: '#app2',
+  components: {
+    'base-layout': compB,
+  },
+});
+
+// const compC = {
+//   props: [],
+//   template: `
+//     <span>
+//       <slot v-bind:user="user">
+//         {{ user.lastName }}
+//       </slot>
+//     </span>
+//   `,
+// };
+
+// const app3 = new Vue({
+//   el: '#app3',
+//   data: {
+//     user: {
+//       firstName: 'Kenta',
+//       lastName: 'Yamaguchi',
+//     },
+//   },
+//   components: {
+//     'current-user': compC,
+//   },
+// });
