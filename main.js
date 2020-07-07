f65b7be68d1502e86f0622ac12f189fd31e3cfb5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-07 21:25:38 +0900

    State Transitions

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
-const app = new Vue({
-  el: '#app',
+new Vue({
+  el: '#animated-number-demo',
   data: {
-    show: true,
+    number: 0,
+    tweenedNumber: 0
   },
-});
+  computed: {
+    animatedNumber: function() {
+      return this.tweenedNumber.toFixed(0);
+    }
+  },
+  watch: {
+    number: function(newValue) {
+      gsap.to(this.$data, { duration: 0.5, tweenedNumber: newValue });
+    }
+  }
+})
+0
 
-const app2 = new Vue({
-  el: '#app2',
-  data: {
-    show: true,
-  },
-});
+var Color = net.brehaut.Color
 
-const app3 = new Vue({
-  el: '#app3',
+new Vue({
+  el: '#example-7',
   data: {
-    show: true,
+    colorQuery: '',
+    color: {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 1
+    },
+    tweenedColor: {}
   },
-});
+  created: function () {
+    this.tweenedColor = Object.assign({}, this.color)
+  },
+  watch: {
+    color: function () {
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+      
+      new TWEEN.Tween(this.tweenedColor)
+        .to(this.color, 750)
+        .start()
+      
+      animate()
+    }
+  },
+  computed: {
+    tweenedCSSColor: function () {
+      return new Color({
+        red: this.tweenedColor.red,
+        green: this.tweenedColor.green,
+        blue: this.tweenedColor.blue,
+        alpha: this.tweenedColor.alpha
+      }).toCSS()
+    }
+  },
+  methods: {
+    updateColor: function () {
+      this.color = new Color(this.colorQuery).toRGB()
+      this.colorQuery = ''
+    }
+  }
+})
